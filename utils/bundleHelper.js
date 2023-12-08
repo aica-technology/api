f5606287033ee537d53d3f658cff3c17c2bb3c9e
jjenscodee <yihsin.jen@gmail.com>
2023-12-08 11:28:52 +0100

    fix: dereference schema

--- utils/bundleHelper.js
+++ utils/bundleHelper.js
@@ -3,13 +3,23 @@ import * as fs from "fs";
 
 let filePath = process.argv[2];
 let outputFileName = process.argv[3];
+
 $RefParser.bundle(filePath).then(async (schema) => {
+
     let jsonSchema = decodeURI(JSON.stringify(schema, null, 2));
     jsonSchema = jsonSchema.replaceAll('%24', '$');
-    fs.writeFile(outputFileName, jsonSchema, 'utf8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
+
+    $RefParser.dereference(JSON.parse(jsonSchema), {
+      dereference: {
+        circular: 'ignore',
+      }}).then((derefSchema) => {
+
+          fs.writeFile(outputFileName, JSON.stringify(derefSchema, null, 2), 'utf8', (err) => {
+              if (err) throw err;
+            console.log('The file has been saved!');
+          });
+    })
+
 });
 
 
