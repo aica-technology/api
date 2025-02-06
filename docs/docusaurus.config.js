2093acc670e6d155f87d2d4f0f2a4c143f90c5cb
Enrico Eberhard <enrico.eberhard@gmail.com>
2025-02-06 12:16:34 +0100

    docs: improve links on homepage, header and footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,133 +7,148 @@ const url = "https://docs.aica.tech";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "AICA for Developers",
-  tagline:
-    "Documentation for the AICA System and robotics software framework",
-  favicon: "img/favicon.ico",
+    title: "AICA for Developers",
+    tagline:
+        "Documentation for the AICA System and robotics software framework",
+    favicon: "img/favicon.ico",
 
-  url,
-  baseUrl: "/",
+    url,
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  organizationName: "aica-technology",
-  projectName: "api",
+    // GitHub pages deployment config.
+    organizationName: "aica-technology",
+    projectName: "api",
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./src/layout/sidebars.js"),
-          sidebarCollapsed: false,
-          editUrl: "https://github.com/aica-technology/api/tree/main/docs",
-        },
-        blog: false,
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./src/layout/sidebars.js"),
+                    sidebarCollapsed: false,
+                    editUrl: "https://github.com/aica-technology/api/tree/main/docs",
+                },
+                blog: false,
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          hideable: true,
-        },
-      },
-      // TODO: add a social media card
-      // image: "img/docusaurus-social-card.jpg",
-      navbar: {
-        title: "Documentation",
-        logo: {
-          alt: "AICA Logo",
-          src: "img/logo.svg",
-          srcDark: "img/logo_dark.svg",
-        },
-        items: [
-          // TODO: add version dropdown when needed
-          // {
-          //   type: "docsVersionDropdown",
-          //   position: "left",
-          //   dropdownActiveClassDisabled: true,
-          // },
-          {
-            type: "docSidebar",
-            sidebarId: "conceptsSidebar",
-            position: "left",
-            label: "Concepts",
-          },
-          {
-            type: "docSidebar",
-            sidebarId: "gettingStartedSidebar",
-            position: "left",
-            label: "Getting started",
-          },
-          {
-            type: "docSidebar",
-            sidebarId: "programmingReferenceSidebar",
-            position: "left",
-            label: "Programming reference",
-          },
-          {
-            href: `${url}/api`,
-            label: "REST API",
-            position: "left",
-          },
-          {
-            href: "https://www.aica.tech",
-            label: "Website",
-            position: "right",
-          },
-          // TODO: need to apply to DocSearch https://docusaurus.io/docs/search
-          {
-            type: "search",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "More",
-            items: [
-              {
-                label: "Website",
-                href: "https://www.aica.tech",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/aica-technology",
-              },
-              {
-                label: "Contact",
-                href: "mailto:contact@aica.tech",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} AICA SA`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        defaultLanguage: "python",
-        additionalLanguages: ["toml"],
-      },
-    }),
+        ({
+            docs: {
+                sidebar: {
+                    hideable: true,
+                },
+            },
+            // TODO: add a social media card
+            // image: "img/docusaurus-social-card.jpg",
+            navbar: {
+                title: "Documentation",
+                logo: {
+                    alt: "AICA Logo",
+                    src: "img/logo.svg",
+                    srcDark: "img/logo_dark.svg",
+                },
+                items: [
+                    // TODO: add version dropdown when needed
+                    // {
+                    //   type: "docsVersionDropdown",
+                    //   position: "left",
+                    //   dropdownActiveClassDisabled: true,
+                    // },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "conceptsSidebar",
+                        position: "left",
+                        label: "Concepts",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "gettingStartedSidebar",
+                        position: "left",
+                        label: "Getting started",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "programmingReferenceSidebar",
+                        position: "left",
+                        label: "Programming reference",
+                    },
+                    {
+                        href: `${url}/api`,
+                        label: "REST API",
+                        position: "left",
+                    },
+                    {
+                        href: "https://www.github.com/aica-technology",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                    {
+                        href: "https://aica.thinkific.com",
+                        label: "AICAdemy",
+                        position: "right",
+                    },
+                    {
+                        href: "https://www.aica.tech",
+                        label: "Website",
+                        position: "right",
+                    },
+                    // TODO: need to apply to DocSearch https://docusaurus.io/docs/search
+                    {
+                        type: "search",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "More",
+                        items: [
+                            //https://github.com/orgs/aica-technology/discussions
+                            {
+                                label: "Discussions, announcements, and new releases",
+                                href: "https://github.com/orgs/aica-technology/discussions",
+                            },
+                            {
+                                label: "Report an issue",
+                                href: "https://github.com/aica-technology/api/issues/new/choose",
+                            },
+                            {
+                                label: "Website",
+                                href: "https://www.aica.tech",
+                            },
+                            {
+                                label: "Contact",
+                                href: "mailto:contact@aica.tech",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} AICA SA`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                defaultLanguage: "python",
+                additionalLanguages: ["toml"],
+            },
+        }),
 };
 
 module.exports = config;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,39 +1,64 @@
 import React from "react";
 import clsx from "clsx";
-// import Link from "@docusaurus/Link";
+import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
 
+type LinkButtonProps = {
+    link: string;
+    text: string
+};
+
+function LinkButton({link, text}: LinkButtonProps) {
+    return <Link
+        className="button button--outline button--secondary button--lg"
+        to={link}
+    >
+        {text}
+    </Link>
+}
+
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          {/* <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link> */}
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.centered}>
+                    <LinkButton link={"/docs/concepts/intro"} text={"Learn basic concepts"}/>
+                    <LinkButton link={"/docs/getting-started/intro"} text={"Get up and running"}/>
+                    <LinkButton link={"/docs/reference/intro"} text={"Advanced programming"}/>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+function AICAdemySection() {
+    return (
+        <header className={clsx("hero", styles.heroBanner)}>
+            <div className="container">
+                <p className="hero__subtitle">
+                    Visit our learning platform for more video lessons and training modules
+                </p>
+                <LinkButton link={"/docs/intro"} text={"AICAdemy"}/>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  return (
-    <Layout>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+                <AICAdemySection/>
+            </main>
+        </Layout>
+    );
 }
