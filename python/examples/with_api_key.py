c072c5e87bca2d9cbbe90642aa798d8b5d5626e5
Louis Brunner <louis.brunner.fr@gmail.com>
2025-10-14 14:28:08 +0100

    improve CI, fix build

--- python/examples/with_api_key.py
+++ python/examples/with_api_key.py
@@ -7,7 +7,7 @@ client = AICA(
     api_key=os.getenv('AICA_API_KEY'),
 )
 
-print(f"Check: {"pass" if client.check() else "failed"}")
+print(f'Check: {"pass" if client.check() else "failed"}')
 print(f'Core Version: {client.core_version()}')
 print(f'Protocol: {client.protocol()}')
 print(f'Application state: {client.get_application_state()}')
@@ -15,4 +15,4 @@ try:
     print(f'Load component: {client.load_component("def")}')
 except:  # noqa: E722
     logging.exception('load component failed')
-print(f'Wait for component: {client.wait_for_component('abc', 'loaded', 5)}')
+print(f'Wait for component: {client.wait_for_component("abc", "loaded", 5)}')
