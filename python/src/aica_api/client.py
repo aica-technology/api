24f07ec8c7cb9623dbe6c8a6e95f166f3ca7cfa7
Sergei Zobov <szobov@pop-os.fritz.box>
2025-11-14 09:59:28 +0100

    aica_api/client.py: remove duplicated bool

--- python/src/aica_api/client.py
+++ python/src/aica_api/client.py
@@ -71,7 +71,7 @@ from aica_api.sio_client import read_until
 CLIENT_VERSION = importlib.metadata.version('aica_api')
 
 T = TypeVar('T')
-ValueParamaterT: TypeAlias = Union[bool, int, float, bool, str,
+ValueParamaterT: TypeAlias = Union[bool, int, float, str,
                                    list[bool], list[int], list[float], list[str]] 
 
 
