e98d7f257a9626f6ee624edb26b019407333e037
Sergei Zobov <szobov@pop-os.fritz.box>
2025-11-14 09:59:58 +0100

    aica_api/client.py: typo

--- python/src/aica_api/client.py
+++ python/src/aica_api/client.py
@@ -71,7 +71,7 @@ from aica_api.sio_client import read_until
 CLIENT_VERSION = importlib.metadata.version('aica_api')
 
 T = TypeVar('T')
-ValueParamaterT: TypeAlias = Union[bool, int, float, str,
+ValueParameterT: TypeAlias = Union[bool, int, float, str,
                                    list[bool], list[int], list[float], list[str]] 
 
 
@@ -527,7 +527,7 @@ class AICA:
         self,
         component: str,
         parameter: str,
-        value: ValueParamaterT,
+        value: ValueParameterT,
     ) -> None:
         """
         Set a parameter on a component.
@@ -552,7 +552,7 @@ class AICA:
         hardware: str,
         controller: str,
         parameter: str,
-        value: ValueParamaterT,
+        value: ValueParameterT,
     ) -> None:
         """
         Set a parameter on a controller.
