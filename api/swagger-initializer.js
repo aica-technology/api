4e1b943f3b78c8ced7c8dbfb3a7864c4d2020ae9
eeberhard <eeberhard@users.noreply.github.com>
2026-09-22 15:02:15 +0000

    deploy: 8ad08e119b78dab2448a305aa3bd3a68739697b6

--- api/swagger-initializer.js
+++ api/swagger-initializer.js
@@ -1,20 +1,64 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+const PythonRequestsPlugin = {
+	fn: {
+		requestSnippetGenerator_python_requests: (request) => {
+			const url = request.get("url");
+			const method = request.get("method").toLowerCase();
+			const body = request.get("body");
+			const headers = request
+				.get("headers")
+				.map((value, key) => `    "${key}": "${value}"`)
+				.join(",\n");
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "./schema.yaml",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+			let data = "";
+			if (body) {
+				try {
+					const parsedBody = JSON.parse(body);
+					const json = Object.entries(parsedBody)
+						.map(([key, value]) => `    "${key}": ${JSON.stringify(value)}`)
+						.join(",\n");
+					data = `  json={\n${json}\n  },\n`;
+				} catch (e) {
+					data = `  data="${body}",\n`;
+				}
+			}
 
-  //</editor-fold>
+			return `import requests
+
+response = requests.${method}(
+  "${url}",
+  headers={
+${headers}
+  },
+${data})
+
+print(response.text)`;
+		},
+	},
+};
+
+window.onload = () => {
+	const baseURL = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+	window.ui = SwaggerUIBundle({
+		url: `${baseURL}schema.yaml`,
+		dom_id: "#swagger-ui",
+		deepLinking: true,
+		filter: true,
+		presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+		plugins: [PythonRequestsPlugin],
+		layout: "BaseLayout",
+		responseInterceptor: (response) => {
+			response.text = response.text.replace(/__AICA_LOCAL_SERVER__/g, baseURL);
+			return response;
+		},
+		requestSnippetsEnabled: true,
+		requestSnippets: {
+			generators: {
+				python_requests: {
+					title: "Python (requests)",
+					syntax: "python",
+				},
+			},
+			languages: ["curl_bash", "python_requests"],
+		},
+	});
 };
